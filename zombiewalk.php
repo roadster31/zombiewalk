c2e74e7615326ee6e05a64c5736a8da2840386a3
Loick Virot <virot.loick@gmail.com>
2017-10-31 20:50:57 +0100

    create entry point

---
+++ zombiewalk.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loick
+ * Date: 31/10/17
+ * Time: 20:12
+ */
+
+use Aliment\AlimentFactory;
+
+echo "
+                               ..... \n           
+                               C C  /\n            
+                              /<   / \n            
+               ___ __________/_#__=o \n            
+              /(- /(\_\________   \  \n            
+              \ ) \ )_      \o     \ \n            
+              /|\ /|\       |'     | \n            
+                            |     _| \n            
+                            /o   __\ \n            
+                           / '     | \n            
+                          / /      | \n            
+                         /_/\______| \n            
+                        (   _(    <  \n            
+                         \    \    \ \n            
+                          \    \    |\n            
+                           \____\____\ \n          
+                           ____\_\__\_\ \n          
+                         /`   /`     o\  \n        
+                         |___ |_______|. \n
+";
+echo "Bonjour. Je vous présente Jean Jacques.\n";
+echo "Vous verrez, il est très sympa.";
+echo "Vous pouvez lui donner à manger. Attention, il est allergique aux poivrons, ail et la banane.\n";
+echo "Mais il adore la pizza.\n";
+
+const PIZZA = "piz";
+const AIL = "ail";
+const POIVRON = "poi";
+const BANANE = "ban";
+
+
+while(1) {
+    $userInput = readline("Que voulez vous donner à manger au zombie ?");
+    processUserInput($userInput);
+}
+
+function processUserInput(String $user) {
+    $factory = new AlimentFactory();
+    try {
+        $aliment = $factory->getAliment($user);
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+    }
+}
